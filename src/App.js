763838c070d3a14334db777318aa55196c53e790
Austin <austinjpoulsen@gmail.com>
2024-11-14 16:54:30 -0700

    another update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import Nav from './components/Nav';
 import Footer from './components/Footer'
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Books from './pages/Books';
 import BooksInfo from "./pages/BookInfo"
 import { books } from "./data"
+import Cart from './pages/Cart';
 
 
 function App() {
+  const [cart, setCart] = useState([])
+
+  function addToCart(book) {
+    setCart([...cart, book])
+   }
+
+  useEffect(() =>{
+    console.log(cart);
+  }, [cart])
+
   return (
     <Router>
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
       <Route path="/books" exact render={() => <Books books={books} />} />
-      <Route path="/books:id" render={() => <BooksInfo books={books} />} />
+      <Route path="/books/:id" render={() => <BooksInfo books={books} addToCart={addToCart}/>} />
+      <Route path="/cart" render={() => <Cart books={books} />} />
       <Footer />
     </div>
     </Router>
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,12 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Rating from "../components/ui/Rating";
 import Price from "../components/ui/Price";
+import Book from "../components/ui/Book";
 
-const BookInfo = ({ books }) => {
+const BookInfo = ({ books, addToCart }) => {
   const { id } = useParams();
-  const book = books.find(book => +book.id === +id);
+  const book = books.find((book) => +book.id === +id);
+
+  function addBookToCart(book) {
+    addToCart(book);
+  }
+
+  function bookExistsOnCart() {
+    return cart.find(book => book.id === +id);
+  }
 
   return (
     <div id="books__body">
@@ -23,34 +32,39 @@ const BookInfo = ({ books }) => {
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img
-                  src={book.url}
-                  alt=""
-                  className="book__selected--ig"
-                />
+                <img src={book.url} alt="" className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">
-                  {book.title}
-                </h2>
+                <h2 className="book__selected--title">{book.title}</h2>
                 <Rating rating={book.rating} />
                 <div className="book__selected--price">
-                  <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
                 </div>
                 <div className="book__summary">
-                  <h3 className="book_summary--title">
-                    Summary
-                  </h3>
+                  <h3 className="book_summary--title">Summary</h3>
                   <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quo itaque debitis, non assumenda, minus tempora eius ipsam beatae architecto quia id nostrum vel dicta recusandae sed repudiandae dignissimos! Nisi.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Suscipit quo itaque debitis, non assumenda, minus tempora
+                    eius ipsam beatae architecto quia id nostrum vel dicta
+                    recusandae sed repudiandae dignissimos! Nisi.
                   </p>
                   <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quo itaque debitis, non assumenda, minus tempora eius ipsam beatae architecto quia id nostrum vel dicta recusandae sed repudiandae dignissimos! Nisi.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Suscipit quo itaque debitis, non assumenda, minus tempora
+                    eius ipsam beatae architecto quia id nostrum vel dicta
+                    recusandae sed repudiandae dignissimos! Nisi.
                   </p>
                 </div>
-                <button className="btn">
-                  Add to cart
-                </button>
+                {bookExistsOnCart() ? (
+                  <button className="btn">Checkout</button>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
+                    Add to cart
+                  </button>
+                )}
               </div>
             </div>
           </div>
@@ -58,11 +72,16 @@ const BookInfo = ({ books }) => {
         <div className="books__container">
           <div className="row">
             <div className="book__selected--top">
-              <h2 className="book__selected--title--top">
-                Recommended Books
-              </h2>
+              <h2 className="book__selected--title--top">Recommended Books</h2>
             </div>
-
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+            </div>
           </div>
         </div>
       </main>
